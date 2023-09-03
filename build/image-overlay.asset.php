caababe5cc36f6c64fb46f0cf92731ec15e59d7c
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2023-09-03 07:29:45 -0400

    Run build and create zip

---
+++ build/image-overlay.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '53fcf5a848a49e71a800');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'bfb3fb46188ac78b9b34');
